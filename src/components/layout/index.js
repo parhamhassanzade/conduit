c59b8c340581a2dab36ce950c84ef888ef11fbb9
parham <parhamhassanzade.p@gmail.com>
2023-01-23 02:36:08 +0330

    delete un used items

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from './Header'
-import Footer from './Footer'
 function Layout({ children }) {
     return (
         <>
             <Header />
             {children}
-            {/* <Footer /> */}
         </>
     )
 }
